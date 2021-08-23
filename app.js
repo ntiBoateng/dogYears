bf73edb7da78a8e6ca912b7f9f18906be6d2978a
Gideon Nti Boateng <gideon.boateng@amalitech.org>
2021-08-22 18:21:51 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+//my age in years
+let myAge = 30;
+//this is a comment
+let earlyYears = 2;
+earlyYears*=10.5;
+//This is a comment
+let laterYears = myAge-2;
+//This is a comment
+laterYears*=4;
+console.log(earlyYears);
+console.log(laterYears);
+//my age in dog years
+var myAgeInDogYear = earlyYears + laterYears;
+//this is a comment
+var myName = 'Gideon'.toLowerCase();
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYear} years old in dog years.`);
